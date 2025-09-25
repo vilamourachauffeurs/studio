47cb0a1304c10bfd68f18fdae9ec90bd126386ca
Vilamoura Chauffeurs <vilamourachauffeurs@gmail.com>
2025-09-25 22:54:04 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         <AuthProvider>
           {children}
           <Toaster />
