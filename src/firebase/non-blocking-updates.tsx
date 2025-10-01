3b9ea5900c0b14ed62e71324a3b12efa1c65373f
Vilamoura Chauffeurs <vilamourachauffeurs@gmail.com>
2025-10-01 23:14:54 +0000

    Try fixing this error: `Console Error: Error: col_ref is not defined. Er

--- src/firebase/non-blocking-updates.tsx
+++ src/firebase/non-blocking-updates.tsx
@@ -38,7 +38,7 @@ export function setDocumentNonBlocking(docRef: DocumentReference, data: any, opt
  * Returns the Promise for the new doc ref, but typically not awaited by caller.
  */
 export function addDocumentNonBlocking(colRef: CollectionReference, data: any) {
-  const promise = addDoc(col_ref, data)
+  const promise = addDoc(colRef, data)
     .catch(error => {
       errorEmitter.emit(
         'permission-error',
