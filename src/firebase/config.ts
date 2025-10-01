cb1e89cfaae929a46dc1fe46d1a06433e75319eb
Vilamoura Chauffeurs <vilamourachauffeurs@gmail.com>
2025-10-01 22:13:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7106436978-f5ba3",
+  "appId": "1:680211956159:web:6ea06cc2d621a5b68d610a",
+  "apiKey": "AIzaSyDhP0rNyGi2dODgIXwZFtC3QzIxPFs69ps",
+  "authDomain": "studio-7106436978-f5ba3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "680211956159"
+};
