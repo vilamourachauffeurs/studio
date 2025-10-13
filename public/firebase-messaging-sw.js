52dced7e480f8db6cbeca72289dffec29bece876
Vilamoura Chauffeurs <vilamourachauffeurs@gmail.com>
2025-10-13 23:01:00 +0000

    I've assigned the booking to the driver catarina_sofia6@hotmail.com , wh

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+
+// Initialize the Firebase app in the service worker
+// "Default" Firebase configuration (prevents errors)
+const firebaseConfig = {
+    apiKey: "AIzaSyDhP0rNyGi2dODgIXwZFtC3QzIxPFs69ps",
+    authDomain: "studio-7106436978-f5ba3.firebaseapp.com",
+    projectId: "studio-7106436978-f5ba3",
+    storageBucket: "studio-7106436978-f5ba3.appspot.com",
+    messagingSenderId: "680211956159",
+    appId: "1:680211956159:web:6ea06cc2d621a5b68d610a"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
