300cbc0a18e9155c38f095c76095ba2599cd9231
Vilamoura Chauffeurs <vilamourachauffeurs@gmail.com>
2025-10-14 08:32:27 +0000

    Try fixing this error: `Console Error: Error: 2 UNKNOWN: Getting metadat

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,32 +1,34 @@
+// This file is required to handle web push notifications in the background.
+// It must be in the public directory.
+
 // Scripts for firebase and firebase messaging
-importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js");
+
 
 // Initialize the Firebase app in the service worker
-// "Default" Firebase configuration (prevents errors)
-const firebaseConfig = {
-    apiKey: "AIzaSyDhP0rNyGi2dODgIXwZFtC3QzIxPFs69ps",
-    authDomain: "studio-7106436978-f5ba3.firebaseapp.com",
-    projectId: "studio-7106436978-f5ba3",
-    storageBucket: "studio-7106436978-f5ba3.appspot.com",
-    messagingSenderId: "680211956159",
-    appId: "1:680211956159:web:6ea06cc2d621a5b68d610a"
-};
+// Be sure to replace the config values with your own
+firebase.initializeApp({
+  apiKey: "AIzaSyDhP0rNyGi2dODgIXwZFtC3QzIxPFs69ps",
+  authDomain: "studio-7106436978-f5ba3.firebaseapp.com",
+  projectId: "studio-7106436978-f5ba3",
+  storageBucket: "studio-7106436978-f5ba3.appspot.com",
+  messagingSenderId: "680211956159",
+  appId: "1:680211956159:web:6ea06cc2d621a5b68d610a"
+});
 
-firebase.initializeApp(firebaseConfig);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log('Received background message ', payload);
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+    };
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
 });
